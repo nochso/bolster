706736e9cf32de1d4a39732c7e9f9e1105433dab
Marcel Voigt <mv@noch.so>
2017-05-06 17:25:12 +0200

    Improve codec roundtrip test

--- codec/internal/test.go
+++ codec/internal/test.go
@@ -25,27 +25,45 @@ type testStruct struct {
 	L float32
 	M float64
 	N string
+	O testStructChild
+	P *testStructChild
+}
+
+type testStructChild struct {
+	A int
+	B bool
 }
 
 func Roundtrip(t *testing.T, c codec.Interface) {
 	r := rand.New(rand.NewSource(0))
 	for i := 0; i < 1000; i++ {
+		// Create random struct
 		expr, ok := quick.Value(reflect.TypeOf(testStruct{}), r)
 		if !ok {
 			t.Fatal("unable to create random struct")
 		}
 		exp := expr.Addr().Interface()
+		// Encode to bytes
 		b, err := c.Marshal(exp)
 		if err != nil {
 			t.Fatal(err)
 		}
-		var act testStruct
-		err = c.Unmarshal(b, &act)
+		// Decode it back to a new struct
+		act := &testStruct{}
+		err = c.Unmarshal(b, act)
 		if err != nil {
 			t.Fatal(err)
 		}
 		if !reflect.DeepEqual(act, exp) {
-			pretty.Compare(act, exp)
+			t.Error(pretty.Compare(act, exp))
+		}
+		// Encode to bytes again
+		b2, err := c.Marshal(act)
+		if err != nil {
+			t.Error(err)
+		}
+		if !reflect.DeepEqual(b, b2) {
+			t.Error(pretty.Compare(b2, b))
 		}
 	}
 }
