8e5f82a0b7e2b9bf670d238f54f2e30bddfabd40
Marcel Voigt <mv@noch.so>
2017-05-13 10:22:37 +0200

    Test state after insert

--- tx_test.go
+++ tx_test.go
@@ -1,11 +1,14 @@
 package bolster_test
 
 import (
+	"flag"
 	"testing"
 
 	"github.com/nochso/bolster"
 	"github.com/nochso/bolster/internal"
 )
+
+var updateGold = flag.Bool("update", false, "update golden test files")
 
 func TestTx_Insert(t *testing.T) {
 	st, closer := internal.OpenTestStore(t)
@@ -21,4 +24,6 @@ func TestTx_Insert(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+	act := internal.DumpStore(st)
+	internal.Gold(t, act, *updateGold)
 }
