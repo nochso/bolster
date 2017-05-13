6ca26de7c84035c80128c98f7f58f4267e24e006
Marcel Voigt <mv@noch.so>
2017-05-13 10:22:53 +0200

    Comment on codec.Interface

--- codec/codec.go
+++ codec/codec.go
@@ -1,5 +1,9 @@
 package codec
 
+// Interface is implemented by bolster codecs.
+//
+// A bolster codec can marshal a struct to bytes and unmarshal bytes back into
+// a struct of the same type.
 type Interface interface {
 	Marshal(v interface{}) ([]byte, error)
 	Unmarshal(b []byte, v interface{}) error
