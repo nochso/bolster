aee7eabb24e881356f36ce6f4ae1cd19b62d1216
Marcel Voigt <mv@noch.so>
2017-05-13 10:01:34 +0200

    Add autoincrement flag

--- store.go
+++ store.go
@@ -14,8 +14,9 @@ import (
 )
 
 const (
-	tagBolster = "bolster"
-	tagID      = "id"
+	tagBolster       = "bolster"
+	tagID            = "id"
+	tagAutoIncrement = "inc"
 )
 
 type Store struct {
@@ -94,15 +95,17 @@ func (s *Store) register(v interface{}) error {
 }
 
 type typeInfo struct {
-	FullName []byte
-	IDField  int
-	Type     reflect.Type
+	FullName      []byte
+	IDField       int
+	AutoIncrement bool
+	Type          reflect.Type
 }
 
 func newTypeInfo(t reflect.Type) (typeInfo, error) {
 	ti := &typeInfo{
 		FullName: []byte(t.PkgPath() + "." + t.Name()),
 		Type:     t,
+		IDField:  -1,
 	}
 	err := ti.validateIDField()
 	if err != nil {
@@ -113,21 +116,53 @@ func newTypeInfo(t reflect.Type) (typeInfo, error) {
 }
 
 func (ti *typeInfo) validateIDField() error {
-	for i := 0; i < ti.Type.NumField(); i++ {
-		f := ti.Type.Field(i)
-		tags := f.Tag.Get(tagBolster)
-		for _, tag := range strings.Split(tags, ",") {
-			if tag == tagID {
-				ti.IDField = i
-				return nil
-			}
-		}
-	}
-	if idField, ok := ti.Type.FieldByName("ID"); ok {
+	tags := newTagList(ti.Type)
+	idKeys := tags.filter(tagID)
+	if len(idKeys) > 1 {
+		return fmt.Errorf("%v: must not have multiple fields with tag %q", ti, tagID)
+	} else if len(idKeys) == 1 {
+		ti.IDField = idKeys[0]
+	} else if idField, ok := ti.Type.FieldByName("ID"); ok {
 		ti.IDField = idField.Index[0]
+	}
+	if ti.IDField != -1 {
+		ti.AutoIncrement = tags.contains(ti.IDField, tagAutoIncrement)
 		return nil
 	}
 	return fmt.Errorf("%v: unable to find ID field: field has to be named \"ID\" or tagged with `bolster:\"id\"`", ti)
+}
+
+type tagList [][]string
+
+// newTagList returns a list of bolster tags for each struct field.
+func newTagList(rt reflect.Type) tagList {
+	tl := make([][]string, 0, rt.NumField())
+	for i := 0; i < rt.NumField(); i++ {
+		ftags := strings.Split(rt.Field(i).Tag.Get(tagBolster), ",")
+		tl = append(tl, ftags)
+	}
+	return tl
+}
+
+// filter returns the positions of fields containing a tag s.
+func (tl tagList) filter(s string) []int {
+	keys := []int{}
+	for i := range tl {
+		if tl.contains(i, s) {
+			keys = append(keys, i)
+		}
+	}
+	return keys
+}
+
+// contains returns true when i'th field contains tag s.
+func (tl tagList) contains(i int, s string) bool {
+	for _, w := range tl[i] {
+		if w == s {
+			return true
+		}
+	}
+	return false
 }
 
 func (ti *typeInfo) validateBytesort() error {
